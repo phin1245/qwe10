42bdc7539608cd13095281ce4f042e9525136a1f
brad <phin1245@yandex.com>
2022-12-18 04:12:11 +0000

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MongoURI: "mongodb+srv://phin:lager@cluster0.fcdkp4f.mongodb.net/test?ssl=true&replicaSet=atlas-qkusun-shard-0&authSource=admin&retryWrites=true&w=majority"
+};
